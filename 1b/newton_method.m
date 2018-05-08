730a7fc07cde1b247bb546c04dcf0d9412583f88
M46F <muhammadassagaf46@gmail.com>
2018-05-08 12:45:33 +0700

    solution

--- 1b/newton_method.m
+++ 1b/newton_method.m
@@ -1,10 +1,12 @@
-function [X, iter]= newton_method(f,X, max_iter, tol)
-  iter = 0;
-  while iter < max_iter
+function [X, iter, is_converge] = newton_method(f,X, max_iter, tol)
+  iter = 1;
+  is_converge = 0;
+  while iter <= max_iter
     iter += 1;
     X0 = X;
     [H, grad] = hessian(f, X);
     if abs(grad) <= tol
+      is_converge = 1;
       break;
     end
     v = H\-grad;
