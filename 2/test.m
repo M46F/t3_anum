5aeb5e847985b77647389bb135da202ddecce64f
Fersandi Pratama <fersandi_pratama@rocketmail.com>
2018-05-08 22:50:58 +0700

    Add plot and result text file per problem

--- 2/test.m
+++ 2/test.m
@@ -1,18 +1,20 @@
-diary('result.txt')
 diary on
 
 %Test nomor 1
+diary('result1.txt')
 disp('FUNCTION 1')
 testFunction(@nomor1, [1 2]');
 
 %Test nomor 2
+diary('result2.txt')
 disp('FUNCTION 2')
 testFunction(@nomor2, [1 1 -0.5]');
 
 %Test nomor 3
+diary('result3.txt')
 disp('FUNCTION 3')
-x = [-400 -200 200 400   -400 -200 200 400];
-testFunction(@nomor3, [x]');
+x = [-400 -200 200 400 -400 -200 200 400];
+testFunction(@nomor3, x');
 for i = 2:5
     temp = x;
     for n = 1:7
@@ -23,23 +25,23 @@ for i = 2:5
 end
 
 function testFunction(func, x)
-    tols  = [10e-4 10e-6 10e-8 10e-10 10e-12];
+    tols  = [1e-4 1e-6 1e-8 1e-10 1e-12];
     for tol = 1:length(tols)
         disp(['Tolerance : ',num2str(tols(tol))])
         tic;
-        res = lbfgs(func, x, 'StopTol', tols(tol))
-        res.X
+        res = ncg(func, x, 'StopTol', tols(tol))
+        res.X'
         toc;
         disp('--------------------------------------------------------------')
         tic;
-        res = ncg(func, x, 'StopTol', tols(tol))
-        res.X
+        res = lbfgs(func, x, 'StopTol', tols(tol))
+        res.X'
         toc;
         disp('--------------------------------------------------------------')
         tic;
         res = tn(func, x, 'StopTol', tols(tol))
-        res.X
+        res.X'
         toc;
-        disp('--------------------------------------------------------------')  
+        disp('--------------------------x------------------------------------')  
     end
 end
